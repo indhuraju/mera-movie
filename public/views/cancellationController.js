15dc9f365fb9f6ace686ea4e3986dab1c0be2760
indhuraju <indhuraj494@gmail.com>
2017-02-07 21:07:42 +0530

    Add files via upload

--- public/views/cancellationController.js
+++ public/views/cancellationController.js
@@ -1,10 +1,10 @@
 
 'use strict';
 
-module.exports = function($scope, $http) {
+module.exports = function($scope, $http,$log) {
 
 
-  var refresh = function () {
+  var refreshsw = function () {
         $http.get('/sh/sh').success(function (response) {
             console.log('READ IS SUCCESSFUL');
             $scope.showtimelist = response;
@@ -12,14 +12,14 @@ module.exports = function($scope, $http) {
         });
     };
 
-    refresh();
+    refreshsw();
 
     $scope.addShow = function () {
         console.log($scope.showtime);
         $http.post('/sh/sh', $scope.showtime).success(function (response) {
             console.log(response);
             console.log("CREATE IS SUCCESSFUL");
-            refresh();
+            refreshsw();
         });
     };
 
@@ -28,7 +28,7 @@ module.exports = function($scope, $http) {
         $http.delete('/sh/sh/' + id._id).success(function (response) {
             console.log(response);
             console.log('DELETED SUCCESSFULLY');
-            refresh();
+            refreshsw();
         });
     };
 
@@ -43,7 +43,7 @@ module.exports = function($scope, $http) {
         console.log($scope.showtime._id);
         $http.put('/sh/sh/' + $scope.showtime._id, $scope.showtime).success(function (response) {
             console.log(response);
-            refresh();
+            refreshsw();
         })
     }
     // 'use strict';
@@ -62,74 +62,158 @@ module.exports = function($scope, $http) {
     // module.exports = function($scope, $http) {
     //
 
-      var refreshcity = function () {
+      var refreshci = function () {
             $http.get('/c/c').success(function (response) {
-                console.log('READ IS SUCCESSFUL');
                 $scope.citieslist = response;
+                console.log('READ IS SUCCESSFUL');
                 $scope.cities = "";
             });
         };
 
-        refreshcity();
+        refreshci();
 
-        $scope.add = function () {
+        $scope.addCity = function () {
             console.log($scope.cities);
             $http.post('/c/c', $scope.cities).success(function (response) {
                 console.log(response);
                 console.log("CREATE IS SUCCESSFUL");
-                refreshcity();
+                refreshci();
             });
         };
 
-        $scope.remove = function (id) {
+        $scope.removeCity = function (id) {
             console.log(id);
             $http.delete('/c/c/' + id._id).success(function (response) {
                 console.log(response);
                 console.log('DELETED SUCCESSFULLY');
-                refreshcity();
+                refreshci();
             });
         };
 
-        $scope.edit = function (id) {
+        $scope.editCity = function (id) {
              $http.get('/c/c/' + id._id).success(function (response) {
                 $scope.cities = response[0];
             });
         };
 
-        $scope.update = function () {
+        $scope.updateCity = function () {
             console.log("REACHED UPDATE");
             console.log($scope.cities._id);
             $http.put('/c/c/' + $scope.cities._id, $scope.cities).success(function (response) {
                 console.log(response);
-                refreshcity();
+                refreshci();
             })
         }
-    };
-
-// };
 
 
+    var refreshts = function () {
+          $http.get('/t/t').success(function (response) {
+              console.log('READ THEATRE SUCCESSFUL');
+              $scope.thtrslist = response;
+              $scope.thtrs = "";
+          });
+      };
 
-// 'use strict';
-//
-// module.exports = function($scope, TodoService) {
-//   $(document).ready(function()
-//   {
-//       var navItems = $('.admin-menu li > a');
-//       var navListItems = $('.admin-menu li');
-//       var allWells = $('.admin-content');
-//       var allWellsExceptFirst = $('.admin-content:not(:first)');
-//
-//       allWellsExceptFirst.hide();
-//       navItems.click(function(e)
-//       {
-//           e.preventDefault();
-//           navListItems.removeClass('active');
-//           $(this).closest('li').addClass('active');
-//
-//           allWells.hide();
-//           var target = $(this).attr('data-target-id');
-//           $('#' + target).show();
-//       });
-//   });
-// };
+      refreshts();
+
+      $scope.addTr = function () {
+          console.log($scope.thtrs);
+          $http.post('/t/t', $scope.thtrs).success(function (response) {
+              console.log(response);
+              console.log("CREATE THEATRE SUCCESSFUL");
+              refreshts();
+          });
+      };
+
+      $scope.removeTr = function (id) {
+          console.log(id);
+          $http.delete('/t/t/' + id._id).success(function (response) {
+              console.log(response);
+              console.log('DELETED THEATRE SUCCESSFULLY');
+              refreshts();
+          });
+      };
+
+      $scope.editTr = function (id) {
+           $http.get('/t/t/' + id._id).success(function (response) {
+              $scope.thtrs = response[0];
+          });
+      };
+
+      $scope.updateTr = function () {
+          console.log("REACHED THEATRE UPDATE");
+          console.log($scope.thtrs._id);
+          $http.put('/t/t/' + $scope.thtrs._id, $scope.thtrs).success(function (response) {
+              console.log(response);
+              refreshts();
+          })
+      }
+
+
+        $scope.removem = function (id) {
+            console.log(id);
+            $http.delete('/movie/movie/' + id._id).success(function (response) {
+                console.log(response);
+                console.log('DELETED MOVIE SUCCESSFULLY');
+                refreshmv();
+            });
+        };
+
+        $scope.editm = function (id) {
+             $http.get('/movie/movie/' + id._id).success(function (response) {
+                $scope.mov= response[0];
+            });
+        };
+
+        $scope.updatem = function () {
+            console.log("REACHED MOVIE UPDATE");
+            console.log($scope.mov._id);
+            $http.put('/movie/movie/' + $scope.mov._id, $scope.mov).success(function (response) {
+                console.log(response);
+                refreshm();
+            })
+        }
+        var refresh = function() {
+    $http.get('/movie/movie').success(function(response) {
+        console.log('READ IS SUCCESSFUL');
+        $scope.moviList = response;
+        $scope.mov = "";
+    });
+};
+
+refresh();
+
+$scope.addm = function(mov) {
+    $http.get(`http://www.omdbapi.com/?t=${mov.movieTitle}&plot=short&r=json`).success(function(response) {
+        //console.log(response);
+        var movieObj = {};
+        for (var key in response) {
+            if (key == 'Title' || key == 'Year' || key == 'Language' || key == 'Poster' || key == 'Genre' || key == 'Director' || key == 'Actors' ) {
+                movieObj[key] = response[key];
+
+            }
+        }
+
+
+        $http({
+                method: 'POST',
+                url: '/movie/movie',
+                 headers: {'Content-Type': 'application/json'},
+                data: movieObj
+            })
+            .then(function(response) {
+                console.log(response);
+                console.log("CREATE IS SUCCESSFUL");
+                $log.info(response);
+                refresh();
+            });
+
+
+
+
+    });
+    console.log($scope.movi);
+
+};
+
+};
---
+++ public/views/movie-crud.js
@@ -0,0 +1,120 @@
+
+var express = require('express');
+var router = express.Router();
+bodyParser = require('body-parser'), //parses information from POST
+
+
+//Any requests to this controller must pass through this 'use' function
+//Copy and pasted from method-override
+router.use(bodyParser.urlencoded({ extended: true }))
+
+
+var mongoose = require('mongoose');
+// var dbHost = 'mongodb://localhost:27017/test';
+// mongoose.connect(dbHost);
+
+// var movieSchema = mongoose.Schema({
+//   movieID: String,
+//   movieName: String
+//  });
+
+var movieSchema = mongoose.Schema({
+  movieTitle:String,
+  movieYear:String,
+  movieLanguage: String,
+   moviePoster: String,
+  movieGenre: String,
+  movieDirector: String,
+  movieActors: String
+ });
+// var Movie = mongoose.model('Movie', movieSchema, 'movietable');
+
+var Movie = mongoose.model('Movie', movieSchema, 'movietable');
+//
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function(){
+//   console.log("Connected to DB");
+// });
+
+
+// router.get('/movie/getData/', function (req, res,next) {
+//
+//     console.log("REACHED  GET omdb FUNCTION ON SERVER");
+//     Movie.find({}, function (err, docs)
+//     {
+//          res.json(docs);
+//          console.log(docs);
+//
+//     });
+// });
+
+router.get('/movie', function (req, res) {
+    console.log("REACHED GET FUNCTION ON SERVER");
+    Movie.find({}, function (err, docs) {
+         res.json(docs);
+          console.log(docs);
+    });
+});
+
+router.get('/movie/:id', function (req, res) {
+    console.log("REACHED GET ID FUNCTION ON SERVER");
+     Movie.find({_id: req.params.id}, function (err, docs) {
+         res.json(docs);
+
+    });
+});
+
+router.post('/movie', function(req, res){
+  console.log(req.body);
+  var tit = req.body.Title;
+  var yr = req.body.Year;
+  var lan = req.body.Language;
+  var post = req.body.Poster;
+  var gen = req.body.Genre;
+  var dir = req.body.Director;
+  var act = req.body.Actors
+
+  var movie1 = new Movie({
+    movieTitle: tit,
+    movieYear :yr,
+    movieLanguage:lan,
+    moviePoster: post,
+    movieGenre: gen,
+    movieDirector: dir,
+    movieActors: act
+  });
+
+  movie1.save(function(err, docs){
+    if ( err ) throw err;
+    console.log("Book Saved Successfully");
+    res.json(docs);
+  });
+
+  })
+
+router.delete('/movie/:id', function(req, res){
+   console.log("REACHED Delete FUNCTION ON SERVER");
+      Movie.remove({_id:req.params.id}, function(err, docs){
+        res.json(docs);
+    });
+})
+
+router.put('/movie/:id', function(req, res){
+    console.log("REACHED PUT");
+    console.log(req.body);
+    Movie.findOneAndUpdate({_id:req.params.id}, req.body, function (err, data) {
+      res.json(data);
+    });
+})
+
+
+// catch 404 and forward to error handler
+router.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+
+module.exports = router;

ee8b726ec56b858dc641cebb1f0272d8674e5d70
indhuraju <indhuraj494@gmail.com>
2017-02-14 18:52:16 +0530

    Add files via upload

--- public/views/bookingController.js
+++ public/views/bookingController.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = function($scope, $http) {
-  $scope.booking = 'booking';
-
+  // $scope.booking = 'booking';
+$scope.book=false;
 
 var refresh = function() {
 $http.get('/movie/movie').success(function(response) {
@@ -13,4 +13,29 @@ $scope.mov = "";
 };
 
 refresh();
+
+// $scope.bookingmovi = function(m){
+//       console.log(m+ "booking is successfull");
+//     };
+
+$scope.myBook=function(){
+  $scope.book=true;
+}
+
+// function myBook() {
+//   $scope.book=true;
+//     // var x = document.getElementById('myDIV');
+//     // if (x.style.display === 'none') {
+//     //     x.style.display = 'block';
+//     // } else {
+//     //     x.style.display = 'none';
+//     // }
+// }
+
+function mySub() {
+    alert("BOOKING IS SUCCESSFUL");
+}
+
+
+
 }

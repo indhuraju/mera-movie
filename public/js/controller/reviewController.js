262335b9ba385ed67addfba715e700c623f0fbf0
indhuraju <indhuraj494@gmail.com>
2017-02-22 20:20:17 +0530

    Add files via upload

--- public/js/controller/reviewController.js
+++ public/js/controller/reviewController.js
@@ -1,10 +1,11 @@
 'use strict';
 
-module.exports = function($scope, $http,$log, $rootScope) {
+module.exports = function($scope, $http,$log, $rootScope, $location) {
 $scope.m = $rootScope.bookedMovie;
+var revname,j;
 console.log(  $scope.m);
 
-
+var reviewres=[];
 var refreshrev = function () {
       $http.get('/re/re').success(function (response) {
           $scope.reviewlist = response;
@@ -14,40 +15,104 @@ var refreshrev = function () {
 
   refreshrev();
 
-$scope.addrev = function () {
+  // $scope.addrev = function () {
+  //
+  //     $scope.review.movieName=$scope.m.movieTitle;
+  //     console.log($scope.review);
+  //     $http.post('/re/re', $scope.review).success(function (response) {
+  //         console.log(response);
+  //         console.log("Rating IS SUCCESSFUL");
+  //         refreshrev();
+  //     });
+  // };
 
-    $scope.review.movieName=$scope.m.movieTitle;
-    console.log($scope.review);
-    $http.post('/re/re', $scope.review).success(function (response) {
-        console.log(response);
-        console.log("Rating IS SUCCESSFUL");
-        refreshrev();
-    });
+  $scope.addrev = function () {
+
+      $scope.review.movieName=$scope.m.movieTitle;
+      console.log($scope.review);
+      $http.post('/re/re', $scope.review).success(function (response) {
+        revname=$scope.review.movieName;
+                $http.get('/re/re').success(function (response) {
+          console.log(response);
+          reviewres=response;
+          console.log("succesfully get data for avg rating");
+        var count=0,cnt=0;
+                                 var i;
+                                 try
+                                {
+                                    for(i=0;i<=reviewres.length;i++)
+                                    {
+                                      if(reviewres[i].movieName== revname)
+                                      {
+                                        cnt++;
+                                   count+=parseInt(reviewres[i].rating);
+                                    }
+
+                                    }
+
+                                  }
+                                 catch(e){}
+
+                                  if(count>0)
+                                  {
+                                    $scope.ratecount=Math.round(count*100/(cnt*5));
+                                    console.log($scope.ratecount);
+                                    $http.get('/movie/movie').success(function(response) {
+                                    console.log('READ IS SUCCESSFUL');
+                                    $scope.moviList = response;
+                                    $scope.mov = "";
+                                    for(j=0;j<=$scope.moviList.length;j++)
+                                          {
+                                            if($scope.moviList[j].movieTitle== revname){
+                                    console.log($scope.moviList[j]._id);
+
+                                      $scope.moviList[j].avgrate=$scope.ratecount;
+                                      console.log($scope.moviList[j]);
+                                      $http.put('/movie/movie/' + $scope.moviList[j]._id, $scope.moviList[i]).success(function (response) {
+                                          console.log(response);
+                                          refresh5();
+                                        });
+                                      }
+}
+                                    });
+
+                                  }
+                                });
+
+                              });
+
 };
 
-// var sum=0;
-// for (var i=0; i<5; i++)
-// {
-//     sum += this.getField("Rating." + i).value;
-// }
-// event.value = sum / 5;
+var refresh5 = function() {
+$http.get('/movie/movie').success(function(response) {
+console.log('READ IS SUCCESSFUL');
+$scope.moviList = response;
+$scope.mov = "";
+});
+};
+refresh5();
+    //                           //  };
+    // $scope.addavg = function () {
+    //                           // console.log("REACHED UPDATE");
+    //                           var i;
+    //                           for(i=0;i<=$scope.movieObj.length;i++)
+    //                                 {
+    //                                   if($scope.movieObj[i].Title== $scope.movieRw.Title){
+    //                   console.log($scope.movieObj[i]._id);
+    //
+    //                             $scope.movieObj[i].avg=$scope.ratecount;
+    //                             console.log($scope.movieObj[i]);
+    //                             $http.put('/movie/movie/' + $scope.movieObj[i]._id, $scope.movieObj[i]).success(function (response) {
+    //                                 console.log(response);
+    //                                 refresh5();
+    //                               });
+    //                             }
+    //                                 $location.path('/home');
+    //
+    //                   }
+    //                 };
 
-
-function setRating(value, doCallback) {
-      if (value && value < 0 || value > maxRating) { return; }
-      if (doCallback === undefined) { doCallback = true; }
-
-      currentRating = value || currentRating;
-
-      iterate(stars, function(star, index) {
-        if (index < currentRating) {
-          star.classList.add('is-active');
-        } else {
-          star.classList.remove('is-active');
-        }
-      });
-
-      if (callback && doCallback) { callback(getRating()); }
-    }
-
+            // function addrev() {
+            //   alert("Your rating is SUCCESSFUL");
+            // }
 };
